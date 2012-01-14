bbce8dec51218fc614dbb5678035e3e13878e1c0
happy <happy@doc5.com>
2012-01-14 20:58:57 +0800

    desc

--- tmp/bash/happy_doc5_update.sh
+++ tmp/bash/happy_doc5_update.sh
@@ -1,1 +1,3 @@
 cd /home/happy/git_root/doc5.github.com && git pull origin develop && jekyll --lsi
+
+#cron 0   *   *   *   *  /home/happy/running/happy_doc5_update.sh > /home/happy/running/logs/happy_doc5_update.cron
